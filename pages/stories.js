f22b9c9979c8a0dca3f29a7cbe6ac9de61b9eea1
barrosdvlpr <barrosdvlpr@gmail.com>
2022-01-11 09:48:05 -0300

    Add API for the stories

--- pages/stories.js
+++ pages/stories.js
@@ -1,5 +1,36 @@
 import view from '../utils/view.js';
 
-export default function Stories(path) {
-  view.innerHTML = `<div>${path}</div>`;  
+export default async function Stories(path) {
+  const stories = await getStories(path);
+  const hasStories = stories.length > 0;
+                    
+  view.innerHTML = `<div>
+    ${hasStories ? stories.map(story => JSON.stringify(story)) : 'No stories'}
+  </div>`;  
 }
+
+async function getStories(path) {
+  const isHomeRoute = path === '/';
+  const isNewRoute = path === '/new';
+  if (isHomeRoute) {
+    path = '/news';  
+  } else if (isNewRoute) {
+    path = '/newest';  
+  } 
+  const response = await fetch(`https://node-hnapi.herokuapp.com${path}`);
+  const stories = await response.json();
+  return stories;
+}
+
+
+
+
+
+// / (Top) -> /news
+// /new (New) -> /newest
+// /ask (Ask) -> /ask
+// /show (Show) -> /show 
+
+// https://node-hnapi.herokuapp.com
+// Using API:
+// https://scrimba.com/learn/javascript/hacker-news-requesting-data-from-api-endpoint-cEKk9btd
