d34fffbab2529de8461dac4cbffba8ef1ab6cfb0
barrosdvlpr <barrosdvlpr@gmail.com>
2022-01-11 15:32:29 -0300

    Format comment section

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
 import RouterHandler from './router.js'
 
+window.onhashchange = () => {
+  setActiveLink();
+}
+
+function setActiveLink() {
+   const links = document.querySelectorAll('.header-link');
+   links.forEach(link => {
+      const linkPath = link.getAttribute('href');
+      const currentPath = window.location.hash;
+      if (currentPath === linkPath) {
+        link.classList.add('active');  
+      } else {
+        link.classList.remove('active');         
+      }
+   });
+}
+
 class App {
     constructor() {
         new RouterHandler();  
     }  
   } 
 new App();
+
+// Applying underline
+// https://scrimba.com/learn/javascript/hacker-news-formatting-comments-cv83d4sr
