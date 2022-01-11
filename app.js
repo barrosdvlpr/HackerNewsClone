378359bbe76247a582a292d08b142a66e8f31ea6
barrosdvlpr <barrosdvlpr@gmail.com>
2022-01-11 09:33:25 -0300

    Addided router

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
+import RouterHandler from './router.js'
+
 class App {
     constructor() {
-      console.log('app works!')  
+        new RouterHandler();  
     }  
-  }
-  
-  new App();
+  } 
+new App();
